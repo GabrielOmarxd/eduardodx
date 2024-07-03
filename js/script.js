3979aa0dd84b16df64a25a25ea99a3eb9f7b1572
GatoCallcenter <gabriel.uscamayta@unmsm.edu.pe>
2024-07-03 15:19:48 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-document.getElementById('learn-more-btn').addEventListener('click', function() {
-    window.location.href = 'https://www.eduardo.com';
+const postButton = document.getElementById('postButton');
+// const loadingIndicator = document.getElementById('loadingIndicator');
+
+postButton.addEventListener('click', async () => {
+    const url = 'https://longlaoshi-server.shuttleapp.rs/longlaoshi/download-apk';
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = ''; // Let the server dictate the filename
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
 });
